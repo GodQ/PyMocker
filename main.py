f6f73d04c12edf6e034dbfc65b3d83f25116ef09
GodQ <quchuanhao@gmail.com>
2022-07-01 17:34:41 +0800

    update init

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from pymocker.mgmt.mgmt_api import run_mgmt_server
 # from pymocker.proxy.proxy_server import proxy_server
+from pymocker.app_init import db
+
+
+db.create_all()
 
 run_mgmt_server()
